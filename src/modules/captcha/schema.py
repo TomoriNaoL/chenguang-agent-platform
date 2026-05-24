a251063c793ee448e30e6fc6ffa19a40f82f7724
TomoriNaoL <3247942946@qq.com>
2026-05-24 18:09:39 +0800

    delete

--- src/modules/captcha/schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class CaptchaRead(BaseModel):
-    key: str
-    image: str  # data:image/png;base64,...
-
-class CaptchaVerifyRequest(BaseModel):
-    key: str
-    code: str
